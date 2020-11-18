93f1bf888c164a94beeccdab61200220d4a7b845
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-18 16:24:13 -0500

    Update test.sh

--- 9.0/fpm-alpine3.12/test.sh
+++ 9.0/fpm-alpine3.12/test.sh
@@ -8,7 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - drupal *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW test drupal instance via front-end: $ANSI_RESET"
 
-docker run --name some-drupal -p 8080:80 -d quay.io/ibmz/drupal-s390x:9.0
+docker run --name some-drupal -p 8080:80 -d quay.io/ibmz/drupal:9.0
 curl localhost:8080
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
