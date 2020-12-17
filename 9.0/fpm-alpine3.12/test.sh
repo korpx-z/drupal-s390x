98d54ddb1f76cb5eeee4781599ab78e764f7416d
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-12-17 11:46:50 -0500

    Update test.sh

--- 9.0/fpm-alpine3.12/test.sh
+++ 9.0/fpm-alpine3.12/test.sh
@@ -9,7 +9,8 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - drupal *** $ANSI_RESET \n"
 echo -e "$ANSI_YELLOW test drupal instance via front-end: $ANSI_RESET"
 
 docker run --name some-drupal -p 8080:80 -d quay.io/ibm/drupal:9.0
-docker logs some-drupal
+curl localhost:8080
+# testing will not work in travis as firewall forbids connection. This does work on zcx appliances.
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
 
